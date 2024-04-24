40f5e4f17e5f97cff736c4ae1ea2b686fd7d6169
sarojswain680@ <sarojswain680@gmail.com>
2024-04-24 21:23:58 +0530

    Entry file change

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
