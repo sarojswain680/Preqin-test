cff796c4948f32edc95aed62de715c303d806efa
sarojswain680@ <sarojswain680@gmail.com>
2024-04-24 22:37:56 +0530

    build directory added

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.jsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   module: {
